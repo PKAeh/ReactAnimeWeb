c3495fd8e5a958aecb9d6fae33f29a833b4093bd
Panuwat Khannalae <panumataeh@gmail.com>
2022-12-05 23:32:20 +0700

    - create footer - done layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import ThemeProvider from '@mui/material/styles/ThemeProvider'
 import createTheme from '@mui/material/styles/createTheme'
 import './App.css'
 import AnimeAppBar from './components/navigators/AnimeAppBar'
+import Footer from './components/navigators/Footer'
 
 const theme = createTheme({
   palette: {
@@ -19,6 +20,7 @@ function App(): JSX.Element {
     <ThemeProvider theme={theme}>
       <div>
         <AnimeAppBar />
+        <Footer />
       </div>
     </ThemeProvider>
   )
---
+++ src/components/navigators/Footer.tsx
@@ -0,0 +1,47 @@
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Unstable_Grid2'
+
+const Footer = (): JSX.Element => {
+  const goToTop = (): void => window.scrollTo(0, 0)
+
+  return (
+    <Container>
+      <Grid
+        container
+        sx={{
+          alignItems: 'center',
+          padding: '15px 30px',
+          color: 'white',
+          backgroundColor: 'rgba(16, 17, 19,0.9)',
+          cursor: 'pointer'
+        }}
+      >
+        <Grid xs={10}>
+          <Typography sx={{ fontSize: '0.9em', color: 'rgb(160,160, 160)' }}>
+            ดูการ์ตูนออนไลน์ ดูอนิเมะออนไลน์ ตอนใหม่ล่าสุด
+          </Typography>
+        </Grid>
+        <Grid xs={2} sx={{ display: 'flex', justifyContent: 'end' }}>
+          <Grid
+            sx={{
+              display: 'flex',
+              backgroundColor: 'rgb(80, 80, 80)',
+              width: '45px',
+              height: '35px',
+              borderRadius: '8px',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+            onClick={goToTop}
+          >
+            <KeyboardArrowUpIcon />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default Footer
