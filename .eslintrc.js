eab33813095bb5b76c38cbb219054701ff688429
Panuwat Khannalae <panumataeh@gmail.com>
2022-12-05 20:50:01 +0700

    - setup eslin

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,27 +8,62 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
   ],
   settings: {
     react: {
       version: 'detect'
-    }
+    },
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: ['tsconfig.json'],
     ecmaFeatures: {
       jsx: true
     },
     ecmaVersion: 12
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'import'],
   rules: {
+    semi: ['error', 'never'],
     'react/react-in-jsx-scope': 0,
     'react/jsx-uses-react': 0,
     'react/prop-types': 0,
-    semi: ['error', 'never']
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "arrow-body-style": ["error", "as-needed"],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        "prefer": "type-imports",
+      }
+    ],
+    "import/order": [
+      "error",
+      {
+        "groups": [
+          "builtin",
+          "external",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type"
+        ],
+        "pathGroups": [
+          {
+            "pattern": "@/**/**",
+            "group": "parent",
+            "position": "before"
+          }
+        ],
+        "alphabetize": { "order": "asc" }
+      }
+    ],
   }
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-function App() {
+function App(): JSX.Element {
   return <div> HomePage </div>
 }
 
