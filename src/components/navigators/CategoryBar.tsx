9b33c086ad85fe3b4eaf166313213f0d6c2007e0
Panuwat Khannalae <panumataeh@gmail.com>
2022-12-08 22:24:34 +0700

    - create category - layout

--- src/components/navigators/CategoryBar.tsx
+++ src/components/navigators/CategoryBar.tsx
@@ -1,10 +1,38 @@
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
+import { Typography } from '@mui/material'
 import Grid from '@mui/material/Unstable_Grid2'
 import FacebookDummy from './category/FacebookDummy'
 
 const CategoryBar = (): JSX.Element => (
   <Grid>
     <FacebookDummy />
-    <div>Category</div>
+    <Grid sx={{ padding: '5px 10px 5px 20px' }}>
+      <Grid
+        container
+        sx={{
+          alignItems: 'center'
+        }}
+      >
+        <Grid xs={10}>
+          <Typography sx={{ fontWeight: 'bold', color: 'rgb(160,160, 160)' }}>
+            ประเภท / แนวอนิเมะ
+          </Typography>
+        </Grid>
+        <Grid
+          xs={2}
+          container
+          sx={{ justifyContent: 'end', alignItems: 'center' }}
+        >
+          <ArrowDropDownIcon sx={{ fontSize: '1.9em', color: 'white' }} />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{ textAlign: 'center', padding: '10px 13px 10px 0' }}
+        columns={9}
+        spacing={0.5}
+      ></Grid>
+    </Grid>
   </Grid>
 )
 
