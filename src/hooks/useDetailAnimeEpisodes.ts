2c9a30d28e6f455b769efef947cbb14b713b8db1
Panuwat Khannalae <panumataeh@gmail.com>
2022-12-13 23:38:27 +0700

    - create episodeservice and hook

---
+++ src/hooks/useDetailAnimeEpisodes.ts
@@ -0,0 +1,39 @@
+import { useQuery } from '@tanstack/react-query'
+import { getDetailAnimeEpisodes } from './../services/detailAnimeEpsiodes/detailAnimeEpisodesService'
+import type { ApiResponse } from '../services/apiResponse'
+import type { DetailAnimeEpisodesResponse } from '../services/detailAnimeEpsiodes/detailAnimeEpisodesResponse'
+import type { AxiosError, AxiosResponse } from 'axios'
+
+interface useLastAnimePram {
+  detailAnimeEpisodesLoading: boolean
+  detailAnimeEpisodes:
+    | AxiosResponse<ApiResponse<DetailAnimeEpisodesResponse>>
+    | undefined
+  detailAnimeEpisodesError: AxiosError<unknown, unknown> | null
+}
+
+export const useDetailAnimeEpisodes = (
+  page: number,
+  id: string
+): useLastAnimePram => {
+  const limit = 20
+  const offset = (page - 1) * limit
+
+  const {
+    isLoading: detailAnimeEpisodesLoading,
+    data: detailAnimeEpisodes,
+    error: detailAnimeEpisodesError
+  } = useQuery<
+    AxiosResponse<ApiResponse<DetailAnimeEpisodesResponse>>,
+    AxiosError<unknown, unknown> | null
+  >({
+    queryKey: [`animeEpisodes-${id}-${page}`],
+    queryFn: () => getDetailAnimeEpisodes(id, offset, limit)
+  })
+
+  return {
+    detailAnimeEpisodesLoading,
+    detailAnimeEpisodes,
+    detailAnimeEpisodesError
+  }
+}
---
+++ src/services/detailAnimeEpsiodes/detailAnimeEpisodesService.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import type { ApiResponse } from './../apiResponse'
+import type { DetailAnimeEpisodesResponse } from './detailAnimeEpisodesResponse'
+import type { AxiosError, AxiosResponse } from 'axios'
+
+export const getDetailAnimeEpisodes = async (
+  id: string,
+  offset: number,
+  limit: number
+): Promise<AxiosResponse<ApiResponse<DetailAnimeEpisodesResponse>>> =>
+  await axios.get<
+    AxiosError,
+    AxiosResponse<ApiResponse<DetailAnimeEpisodesResponse>>
+  >(
+    `https://kitsu.io/api/edge/episodes?filter[mediaType]=Anime&filter[media_id]=${id}&page[limit]=${limit}&page[offset]=${offset}&sort=number`
+  )
