5c749e7d60d732668ebb40ccf14b7a1a8ec389eb
Panuwat Khannalae <panumataeh@gmail.com>
2022-12-16 00:20:04 +0700

    - create SearchPage - done layout

---
+++ src/components/searchAnime/SearchAnimeNotEmpty.tsx
@@ -0,0 +1,44 @@
+import { Typography } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
+
+interface SearchAnimeNotEmptyProps {
+  search: string
+}
+
+const SearchAnimeNotEmpty = ({
+  search
+}: SearchAnimeNotEmptyProps): JSX.Element => (
+  <Grid sx={{ color: 'rgb(160,160, 160)', padding: '30px 50px' }}>
+    <Grid container>
+      <Grid>
+        <Typography sx={{ fontSize: '2em' }}>
+          ไม่พบสิ่งที่คุณกำลังค้นหา :
+        </Typography>
+      </Grid>
+      <Grid>
+        <Typography
+          sx={{ fontSize: '2em', paddingLeft: '10px', fontWeight: 'bold' }}
+        >
+          {search}
+        </Typography>
+      </Grid>
+    </Grid>
+    <Grid sx={{ paddingLeft: '10px' }}>
+      <Typography
+        sx={{ fontSize: '1.5em', fontWeight: 'bold', paddingTop: '20px' }}
+      >
+        คำแนะนำ :
+      </Typography>
+      <Grid>
+        <Typography sx={{ fontSize: '0.9em' }}>
+          <ul>ตรวจสอบให้แน่ใจว่าสะกดถูกต้อง</ul>
+          <ul>
+            ลองค้นหาเป็นคำๆเช่น เรื่อง Sword Art Online II ลองใส่แค่คำว่า Sword
+          </ul>
+        </Typography>
+      </Grid>
+    </Grid>
+  </Grid>
+)
+
+export default SearchAnimeNotEmpty
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,20 +1,51 @@
-import React from 'react'
+import Grid from '@mui/material/Unstable_Grid2'
+import { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { BaseLoader } from '../components/BaseLoader'
+import BasePagination from '../components/BasePagination'
+import AnimeList from '../components/anime/AnimeList'
+import SearchAnimeNotEmpty from '../components/searchAnime/SearchAnimeNotEmpty'
+import SearchAnimeTitle from '../components/searchAnime/SearchAnimeTitle'
 import { useSearchAnime } from '../hooks/useSearchAnime'
 
 const SearchPage = (): JSX.Element => {
+  const [page, setPage] = useState<number>(1)
   const [searchParams] = useSearchParams()
   const search = searchParams.get('search') ?? ''
   const { searchAnimeLoading, searchAnime, searchAnimeError } = useSearchAnime(
-    1,
+    page,
     search
   )
+  const count = Math.ceil((searchAnime?.meta.count ?? 0) / 40)
+  const onChange = (event: React.ChangeEvent<unknown>, page: number): void => {
+    setPage(page)
+  }
+
+  const isSearchAnimeNotNullNotEmpty =
+    searchAnime !== undefined && searchAnime.data.length > 0
 
   if (searchAnimeLoading) {
     return <BaseLoader style={{ marginTop: '100px' }} />
   }
 
-  return <div>SearchPage</div>
+  return (
+    <Grid sx={{ padding: '10px', width: '100%' }}>
+      <Grid sx={{ padding: '15px 8px' }}>
+        <SearchAnimeTitle search={search} />
+      </Grid>
+      {isSearchAnimeNotNullNotEmpty ? (
+        <>
+          <Grid sx={{ width: '100%' }}>
+            <AnimeList data={searchAnime.data} />
+          </Grid>
+          <Grid sx={{ padding: '15px 0' }}>
+            <BasePagination page={page} count={count} onChange={onChange} />
+          </Grid>
+        </>
+      ) : (
+        <SearchAnimeNotEmpty search={search} />
+      )}
+    </Grid>
+  )
 }
 export default SearchPage
