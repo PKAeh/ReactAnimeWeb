d2687ce149da5e970318232dbf28d2afcb8605e6
Panuwat Khannalae <panumataeh@gmail.com>
2022-12-17 22:55:00 +0700

    - add Base Link

--- src/components/navigators/AnimeAppBar.tsx
+++ src/components/navigators/AnimeAppBar.tsx
@@ -4,50 +4,49 @@ import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import BaseLink from '../BaseLink'
 import SearchInput from './SearchInput'
 
 const AnimeAppBar = (): JSX.Element => (
   <AppBar position="sticky">
     <Container maxWidth="lg" sx={{ padding: '0 0 0 20px !important' }}>
       <Toolbar disableGutters sx={{ alignContent: 'stretch' }}>
-        <Typography
-          variant="h6"
-          noWrap
-          component="a"
-          href="/"
-          sx={{
-            mr: 4,
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            color: 'inherit',
-            textDecoration: 'none',
-            fontSize: '1.8rem'
-          }}
-        >
-          ANIMEJIMI
-        </Typography>
+        <BaseLink to="/">
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{
+              mr: 4,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              color: 'white',
+              fontSize: '1.8rem'
+            }}
+          >
+            ANIMEJIMI
+          </Typography>
+        </BaseLink>
 
-        <Typography
-          variant="h6"
-          noWrap
-          component="a"
-          href="/react-store/"
-          sx={{
-            mr: 2,
-            display: { xs: 'none', md: 'flex' },
-            fontFamily: 'monospace',
-            color: 'inherit',
-            textDecoration: 'none',
-            fontSize: '1rem',
-            cursor: 'pointer',
-            '&:hover': {
-              color: '#fd5529'
-            }
-          }}
-        >
-          อนิเมะที่ชื่นชอบ{' '}
-          <FavoriteIcon sx={{ padding: '3px 0 0 1px', fontSize: '1.1em' }} />
-        </Typography>
+        <BaseLink to="/favorite">
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              color: 'white',
+              fontSize: '1rem',
+              cursor: 'pointer',
+              '&:hover': {
+                color: '#fd5529'
+              }
+            }}
+          >
+            อนิเมะที่ชื่นชอบ{' '}
+            <FavoriteIcon sx={{ padding: '3px 0 0 1px', fontSize: '1.1em' }} />
+          </Typography>
+        </BaseLink>
 
         <Box sx={{ flexGrow: 1 }}></Box>
 
