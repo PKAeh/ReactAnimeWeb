7a2997f36a20fb9f919939428b7dd28662b12ce0
Panuwat Khannalae <panumataeh@gmail.com>
2022-12-18 12:20:06 +0700

    setup rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,76 +1,70 @@
 module.exports = {
-  ignorePatterns: ['.eslintrc.js', '*.config.js'],
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
-    // "plugin:@typescript-eslint/recommended-requiring-type-checking"
-  ],
-  settings: {
-    react: {
-      version: 'detect'
-    },
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
-    },
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['tsconfig.json'],
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12
-  },
-  plugins: ['react', '@typescript-eslint', 'import'],
-  rules: {
-    'indent': ["error", "tab"],
-    'semi': ['error', 'never'],
-    'react/react-in-jsx-scope': 0,
-    'react/jsx-uses-react': 0,
-    'react/prop-types': 0,
-    "@typescript-eslint/explicit-function-return-type": "error",
-    "arrow-body-style": ["error", "as-needed"],
-    "prettier/prettier": [  //or whatever plugin that is causing the clash
-      "error",
-      {
-        "tabWidth": 4
-      }
-    ],
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
-      {
-        "prefer": "type-imports",
-      }
-    ],
-    "import/order": [
-      "error",
-      {
-        "groups": [
-          "builtin",
-          "external",
-          "parent",
-          "sibling",
-          "index",
-          "object",
-          "type"
-        ],
-        "pathGroups": [
-          {
-            "pattern": "@/**/**",
-            "group": "parent",
-            "position": "before"
-          }
-        ],
-        "alphabetize": { "order": "asc" }
-      }
-    ],
-  }
+	ignorePatterns: ['.eslintrc.js', '*.config.js'],
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:prettier/recommended',
+		'plugin:@typescript-eslint/recommended',
+		// "plugin:@typescript-eslint/recommended-requiring-type-checking"
+	],
+	settings: {
+		react: {
+			version: 'detect'
+		},
+		"import/parsers": {
+			"@typescript-eslint/parser": [".ts", ".tsx"]
+		},
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: ['tsconfig.json'],
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 12
+	},
+	plugins: ['react', '@typescript-eslint', 'import'],
+	rules: {
+		'indent': ["error", "tab"],
+		'semi': ['error', 'never'],
+		'react/react-in-jsx-scope': 0,
+		'react/jsx-uses-react': 0,
+		'react/prop-types': 0,
+		"@typescript-eslint/explicit-function-return-type": "error",
+		"arrow-body-style": ["error", "as-needed"],
+		"@typescript-eslint/consistent-type-imports": [
+			"error",
+			{
+				"prefer": "type-imports",
+			}
+		],
+		"import/order": [
+			"error",
+			{
+				"groups": [
+					"builtin",
+					"external",
+					"parent",
+					"sibling",
+					"index",
+					"object",
+					"type"
+				],
+				"pathGroups": [
+					{
+						"pattern": "@/**/**",
+						"group": "parent",
+						"position": "before"
+					}
+				],
+				"alphabetize": { "order": "asc" }
+			}
+		],
+	}
 };
